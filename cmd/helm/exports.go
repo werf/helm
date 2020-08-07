271d64cdb0da27b2978d59f412ce822ef72a739c
Timofey Kirillov <timofey.kirillov@flant.com>
2020-08-07 14:44:50 +0300

    [helm for werf] Make cmd package exportable
     - change "main" package name to "helm_v3";
     - make all top-level helm commands constructors exportable for helm_v3 package.

---
+++ cmd/helm/exports.go
@@ -0,0 +1,31 @@
+package helm_v3
+
+var (
+	Settings = settings
+
+	LoadReleasesInMemory = loadReleasesInMemory
+	Debug                = debug
+	NewRootCmd           = newRootCmd
+
+	NewUninstallCmd  = newUninstallCmd
+	NewDependencyCmd = newDependencyCmd
+	NewGetCmd        = newGetCmd
+	NewHistoryCmd    = newHistoryCmd
+	NewLintCmd       = newLintCmd
+	NewListCmd       = newListCmd
+	NewTemplateCmd   = newTemplateCmd
+	NewRepoCmd       = newRepoCmd
+	NewRollbackCmd   = newRollbackCmd
+	NewInstallCmd    = newInstallCmd
+	NewUpgradeCmd    = newUpgradeCmd
+	NewCreateCmd     = newCreateCmd
+	NewEnvCmd        = newEnvCmd
+	NewPackageCmd    = newPackageCmd
+	NewPluginCmd     = newPluginCmd
+	NewPullCmd       = newPullCmd
+	NewSearchCmd     = newSearchCmd
+	NewShowCmd       = newShowCmd
+	NewStatusCmd     = newStatusCmd
+	NewTestCmd       = newReleaseTestCmd
+	NewVerifyCmd     = newVerifyCmd
+)
