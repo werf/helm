f952ffc5dd9cf14e4c3e73bff0029de9d7f6e265
Timofey Kirillov <timofey.kirillov@flant.com>
2020-09-23 14:26:23 +0300

    [helm for werf] Introduce DeleteNamespace and DeleteHooks uninstall options

--- cmd/helm/uninstall.go
+++ cmd/helm/uninstall.go
@@ -38,6 +38,15 @@ uninstalling them.
 `
 
 func newUninstallCmd(cfg *action.Configuration, out io.Writer) *cobra.Command {
+	return NewUninstallCmd(cfg, out, UninstallCmdOptions{})
+}
+
+type UninstallCmdOptions struct {
+	DeleteNamespace *bool
+	DeleteHooks     *bool
+}
+
+func NewUninstallCmd(cfg *action.Configuration, out io.Writer, opts UninstallCmdOptions) *cobra.Command {
 	client := action.NewUninstall(cfg)
 
 	cmd := &cobra.Command{
@@ -54,6 +63,13 @@ func newUninstallCmd(cfg *action.Configuration, out io.Writer) *cobra.Command {
 			return compListReleases(toComplete, cfg)
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
+			if opts.DeleteHooks != nil {
+				client.DeleteHooks = *opts.DeleteHooks
+			}
+			if opts.DeleteNamespace != nil {
+				client.DeleteNamespace = *opts.DeleteNamespace
+			}
+
 			for i := 0; i < len(args); i++ {
 
 				res, err := client.Run(args[i])
