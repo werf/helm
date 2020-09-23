67dcadf45d7bd37b44a8eaff1733cf889e2c2e41
Timofey Kirillov <timofey.kirillov@flant.com>
2020-09-23 14:26:23 +0300

    [helm for werf] Introduce DeleteNamespace and DeleteHooks uninstall options

---
+++ pkg/kube/extensions.go
@@ -0,0 +1,36 @@
+package kube
+
+import (
+	"context"
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func (c *Client) DeleteNamespace(ctx context.Context, namespace string, opts DeleteOptions) error {
+	cs, err := c.Factory.KubernetesClientSet()
+	if err != nil {
+		return err
+	}
+
+	if err := cs.CoreV1().Namespaces().Delete(ctx, namespace, metav1.DeleteOptions{}); err != nil {
+		return err
+	}
+
+	if opts.Wait {
+		specs := []*ResourcesWaiterDeleteResourceSpec{
+			{ResourceName: namespace, Namespace: "", GroupVersionResource: schema.GroupVersionResource{
+				Group: "v1",
+				Version: "TODO",
+				Resource: "TODO",
+			}},
+		}
+		if err := c.ResourcesWaiter.WaitUntilDeleted(context.Background(), specs, opts.WaitTimeout); err != nil {
+			return fmt.Errorf("waiting until namespace deleted failed: %s", err)
+		}
+	}
+
+	return nil
+}
