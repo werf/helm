bcabb58b630c973367df10f4313510d63e57b4d4
Timofey Kirillov <timofey.kirillov@flant.com>
2020-12-21 15:18:07 +0300

    [werf for helm] Added support for the default ~/.docker config path (or DOCKER_CONFIG variable) for 'helm chart *' commands

--- internal/experimental/registry/client.go
+++ internal/experimental/registry/client.go
@@ -22,7 +22,12 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"path/filepath"
 	"sort"
+
+	"github.com/docker/cli/cli/config"
+	"github.com/docker/docker/pkg/homedir"
 
 	auth "github.com/deislabs/oras/pkg/auth/docker"
 	"github.com/deislabs/oras/pkg/oras"
@@ -52,6 +57,15 @@ type (
 	}
 )
 
+func getDefaultDockerConfigPath() string {
+	configDir := os.Getenv("DOCKER_CONFIG")
+	if configDir == "" {
+		return filepath.Join(homedir.Get(), ".docker", config.ConfigFileName)
+	} else {
+		return filepath.Join(configDir, config.ConfigFileName)
+	}
+}
+
 // NewClient returns a new registry client with config
 func NewClient(opts ...ClientOption) (*Client, error) {
 	client := &Client{
@@ -65,7 +79,7 @@ func NewClient(opts ...ClientOption) (*Client, error) {
 		client.credentialsFile = helmpath.CachePath("registry", CredentialsFileBasename)
 	}
 	if client.authorizer == nil {
-		authClient, err := auth.NewClient(client.credentialsFile)
+		authClient, err := auth.NewClient(client.credentialsFile, getDefaultDockerConfigPath())
 		if err != nil {
 			return nil, err
 		}
