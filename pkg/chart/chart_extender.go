264231cc9eaaaeffee11a49c29996c01534def09
Timofey Kirillov <timofey.kirillov@flant.com>
2021-01-15 13:54:19 +0300

    [helm for werf] Refactor chart.Extender interface
     - Put all loader function callbacks into extender.
     - Fix subchart loading.
     - Refactor chart.Extender interface functions itself.

--- pkg/chart/chart_extender.go
+++ pkg/chart/chart_extender.go
@@ -1,10 +1,24 @@
 package chart
 
-import "text/template"
+import (
+	"text/template"
+
+	"helm.sh/helm/v3/pkg/cli"
+)
+
+type ChartExtenderBufferedFile struct {
+	Name string
+	Data []byte
+}
 
 type ChartExtender interface {
-	SetupChart(c *Chart) error
-	AfterLoad() error
+	ChartCreated(c *Chart) error
+	ChartLoaded(files []*ChartExtenderBufferedFile) error
+	ChartDependenciesLoaded() error
 	MakeValues(inputVals map[string]interface{}) (map[string]interface{}, error)
 	SetupTemplateFuncs(t *template.Template, funcMap template.FuncMap)
+
+	LoadDir(dir string) (bool, []*ChartExtenderBufferedFile, error)
+	LocateChart(name string, settings *cli.EnvSettings) (bool, string, error)
+	ReadFile(filePath string) (bool, []byte, error)
 }
