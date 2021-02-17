cfe92d710823a12cae046419dfd94641d59c7bbf
Timofey Kirillov <timofey.kirillov@flant.com>
2021-02-17 18:17:56 +0300

    [helm for werf] Export envSettings kube client getter and fix namespace getter to use overriden namespace if set

--- pkg/cli/exports.go
+++ pkg/cli/exports.go
@@ -1,5 +1,11 @@
 package cli
 
+import "k8s.io/cli-runtime/pkg/genericclioptions"
+
 func (s *EnvSettings) GetNamespaceP() *string {
 	return &s.namespace
+}
+
+func (s *EnvSettings) GetConfigP() *genericclioptions.RESTClientGetter {
+	return &s.config
 }
