9b37fb5bf6ce6006f47d8f78e86496289aae343d
Timofey Kirillov <timofey.kirillov@flant.com>
2021-06-01 15:28:21 +0300

    [helm for werf] Support insecure http registries for published helm charts: fix

--- cmd/helm/exports.go
+++ cmd/helm/exports.go
@@ -38,18 +38,19 @@ var (
 	NewRegistryCmd   = newRegistryCmd
 
 	// NOTE: following commands has additional options param and defined in corresponding command files
-	//NewTemplateCmd   = newTemplateCmd
-	//NewInstallCmd    = newInstallCmd
-	//NewUpgradeCmd    = newUpgradeCmd
-	//NewUninstallCmd  = newUninstallCmd
-	//NewChartExportCmd = newChartExportCmd
+	// NewTemplateCmd   = newTemplateCmd
+	// NewInstallCmd    = newInstallCmd
+	// NewUpgradeCmd    = newUpgradeCmd
+	// NewUninstallCmd  = newUninstallCmd
+	// NewChartExportCmd = newChartExportCmd
 
 	LoadPlugins = loadPlugins
 )
 
-func NewRegistryClient(debug bool, out io.Writer) (*registry.Client, error) {
+func NewRegistryClient(debug, insecure bool, out io.Writer) (*registry.Client, error) {
 	return registry.NewClient(
 		registry.ClientOptDebug(debug),
+		registry.ClientOptInsecure(insecure),
 		registry.ClientOptWriter(out),
 	)
 }
